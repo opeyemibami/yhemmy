64144804465bed8d206c9d411abf984bc6bfbd84
opeyemibami <bamigbadeopeyemi@gmail.com>
2023-01-24 10:36:38 +0000

    Updates

---
+++ precache-manifest.e2a8bc03dc0f8f0aa42592b08348939d.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d809cc09f24d6d6ff21cd082391e692",
+    "url": "/yhemmy/index.html"
+  },
+  {
+    "revision": "f4b3e2cf9f1ced4f4244",
+    "url": "/yhemmy/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "1313055d6e44c8766bb5",
+    "url": "/yhemmy/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "dc3a5dbf5d3432f8345e",
+    "url": "/yhemmy/static/css/main.37426cb8.chunk.css"
+  },
+  {
+    "revision": "e9de0c2790a26e914b23",
+    "url": "/yhemmy/static/js/2.558ea56c.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/yhemmy/static/js/2.558ea56c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b3e2cf9f1ced4f4244",
+    "url": "/yhemmy/static/js/3.0776c0ee.chunk.js"
+  },
+  {
+    "revision": "1313055d6e44c8766bb5",
+    "url": "/yhemmy/static/js/4.afe11897.chunk.js"
+  },
+  {
+    "revision": "dc3a5dbf5d3432f8345e",
+    "url": "/yhemmy/static/js/main.935a1072.chunk.js"
+  },
+  {
+    "revision": "1f34d6e73861c9525a3c",
+    "url": "/yhemmy/static/js/runtime-main.486c8fa9.js"
+  },
+  {
+    "revision": "4887e6a46e60bb7dbc5ddc5cbe88c568",
+    "url": "/yhemmy/static/media/0.4887e6a4.JPG"
+  },
+  {
+    "revision": "32c404ae625872585fad76cb650734ce",
+    "url": "/yhemmy/static/media/1.32c404ae.JPG"
+  },
+  {
+    "revision": "56b013cf9a97f8c16c10aa6cfc95d7c2",
+    "url": "/yhemmy/static/media/10.56b013cf.JPG"
+  },
+  {
+    "revision": "d213b55a12ebd8ee9a6510ebe19d6d06",
+    "url": "/yhemmy/static/media/11.d213b55a.JPG"
+  },
+  {
+    "revision": "b96f044ccc7be10952505aee09d91ddb",
+    "url": "/yhemmy/static/media/12.b96f044c.JPG"
+  },
+  {
+    "revision": "4b266e08d4d0c988420217bec75289c7",
+    "url": "/yhemmy/static/media/13.4b266e08.JPG"
+  },
+  {
+    "revision": "0ebc2346bf3621064463ada6be7d739a",
+    "url": "/yhemmy/static/media/14.0ebc2346.JPG"
+  },
+  {
+    "revision": "4dbed6df177f2e9673abaac5d13b9bf8",
+    "url": "/yhemmy/static/media/2.4dbed6df.JPG"
+  },
+  {
+    "revision": "bf5f83db42afeb5fe89929d7239ee9c3",
+    "url": "/yhemmy/static/media/3.bf5f83db.JPG"
+  },
+  {
+    "revision": "7a06c4e767db81b5aa9ad1222657ebef",
+    "url": "/yhemmy/static/media/4.7a06c4e7.JPG"
+  },
+  {
+    "revision": "53b9a77b7748f189f7590987c0ace7de",
+    "url": "/yhemmy/static/media/5.53b9a77b.JPG"
+  },
+  {
+    "revision": "0a7b3a7b2e710693c71fad496bcfed29",
+    "url": "/yhemmy/static/media/6.0a7b3a7b.JPG"
+  },
+  {
+    "revision": "8d719abd63ec25bcf83bc7b6cade435a",
+    "url": "/yhemmy/static/media/7.8d719abd.JPG"
+  },
+  {
+    "revision": "0e0b261c72f2db1ec1dc61ce4d8050cc",
+    "url": "/yhemmy/static/media/8.0e0b261c.JPG"
+  },
+  {
+    "revision": "66e7021a1b60639342a78fcd439028d6",
+    "url": "/yhemmy/static/media/9.66e7021a.JPG"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/yhemmy/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "2805ad50325f7ded30aa8dbef97ed1c5",
+    "url": "/yhemmy/static/media/DSNLogo.2805ad50.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/yhemmy/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b4d0fd3611ad63537f17bf9296402ae7",
+    "url": "/yhemmy/static/media/PhDPortfolio.b4d0fd36.jpeg"
+  },
+  {
+    "revision": "39201075e4ed864c48a40943db84c0a6",
+    "url": "/yhemmy/static/media/SETULogo.39201075.webp"
+  },
+  {
+    "revision": "976b48e23fef1bbbf4805d3eb0b10a00",
+    "url": "/yhemmy/static/media/SQL.976b48e2.png"
+  },
+  {
+    "revision": "145c2404c061fecc74a5b930075a61b4",
+    "url": "/yhemmy/static/media/aischaward.145c2404.145c2404.png"
+  },
+  {
+    "revision": "d18f226da54a06664761f841554418df",
+    "url": "/yhemmy/static/media/awsLogo.d18f226d.d18f226d.png"
+  },
+  {
+    "revision": "92a9dde931e6a7cf11fdaf2063afb8a9",
+    "url": "/yhemmy/static/media/campus_leadLogo.92a9dde9.92a9dde9.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/yhemmy/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "2fae516f75c54ccbeb1c3f7d2fcf543a",
+    "url": "/yhemmy/static/media/containers.2fae516f.jpeg"
+  },
+  {
+    "revision": "c2ba4a4abd574d8094c3dabb18f827ee",
+    "url": "/yhemmy/static/media/dataScience.c2ba4a4a.png"
+  },
+  {
+    "revision": "ebf35d7f33ebbd4a4c6cff7e66e21fdb",
+    "url": "/yhemmy/static/media/data_science.ebf35d7f.svg"
+  },
+  {
+    "revision": "143460b93de3ddae3ca632ddb71f5245",
+    "url": "/yhemmy/static/media/deployment.143460b9.png"
+  },
+  {
+    "revision": "53616040518984642f4a850fcc6baad9",
+    "url": "/yhemmy/static/media/deployment.53616040.webp"
+  },
+  {
+    "revision": "8d59f8cb51d9e7825921d9d7350fb72d",
+    "url": "/yhemmy/static/media/gcp.8d59f8cb.webp"
+  },
+  {
+    "revision": "3fabf8339a560d2cb7f6f4549f69515d",
+    "url": "/yhemmy/static/media/microsoft.3fabf833.3fabf833.png"
+  },
+  {
+    "revision": "22362ac04d38b858dafcc28307cf17f5",
+    "url": "/yhemmy/static/media/openSource.22362ac0.png"
+  },
+  {
+    "revision": "f31ae9858685113d49278ac533713d05",
+    "url": "/yhemmy/static/media/prostateCancer.f31ae985.webp"
+  },
+  {
+    "revision": "25fe1a4417fd48826dace785549a78c1",
+    "url": "/yhemmy/static/media/recomender.25fe1a44.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/yhemmy/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/yhemmy/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "c6d6e22575a357bed8b7edc37eb4ef95",
+    "url": "/yhemmy/static/media/tdd.c6d6e225.webp"
+  },
+  {
+    "revision": "f540792b2d78c60cf0becc4d0cbdc6f6",
+    "url": "/yhemmy/static/media/tennis.f540792b.jpg"
+  },
+  {
+    "revision": "f57adf41926c7f3f04f5f34d4c9dc4f5",
+    "url": "/yhemmy/static/media/tf_data_n_deployment.f57adf41.f57adf41.png"
+  },
+  {
+    "revision": "5c703fd42f447ac93893dbe60109050f",
+    "url": "/yhemmy/static/media/tf_dev.5c703fd4.5c703fd4.png"
+  },
+  {
+    "revision": "71bd6ce100a6c82d72cab42c542700ab",
+    "url": "/yhemmy/static/media/topicModelling.71bd6ce1.png"
+  },
+  {
+    "revision": "a1e62fcc184b62f5e8c157b8f08abd3e",
+    "url": "/yhemmy/static/media/udemyLogo.a1e62fcc.a1e62fcc.png"
+  },
+  {
+    "revision": "66bcaa35461057da0e1635a5bff16eac",
+    "url": "/yhemmy/static/media/unilagLogo.66bcaa35.png"
+  },
+  {
+    "revision": "f9064f3a4a3d148bf45033597243b3d9",
+    "url": "/yhemmy/static/media/wineapp.f9064f3a.webp"
+  },
+  {
+    "revision": "5bf815079789ccfba13365ae8a71c612",
+    "url": "/yhemmy/static/media/yabatechLogo.5bf81507.png"
+  },
+  {
+    "revision": "db1dcb69e246cf69fb073e5aed4c17f4",
+    "url": "/yhemmy/static/media/zindi_ambassadorLogo.db1dcb69.db1dcb69.png"
+  },
+  {
+    "revision": "10d800c987d7019676a1b2ce6ea22797",
+    "url": "/yhemmy/static/media/zindiwinner.10d800c9.10d800c9.png"
+  }
+]);
